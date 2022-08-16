dc2e2b54aa26eefffe667d284fc88de33ea6fd09
João Pedro <joaodev84@gmail.com>
2022-08-16 20:58:55 -0300

    Mobile version

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,22 @@ const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+
+    475: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+
+    1199: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
   }
 });
 
@@ -22,6 +38,28 @@ inputsForm.forEach(input => {
   input.addEventListener("focusout", () => {
     if (input.value === "") {
       field.classList.remove("active");
+    }
+  });
+});
+
+const mobileMenuButton = document.querySelector(".toggle-menu");
+const mobileMenu = document.querySelector(".header-menu-mobile");
+mobileMenuButton.addEventListener("click", () => {
+  mobileMenuButton.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+  document.body.classList.toggle("active");
+});
+
+const mobileMenuItems = document.querySelectorAll(
+  ".header-menu-mobile .menu-item"
+);
+
+mobileMenuItems.forEach(menuItem => {
+  menuItem.addEventListener("click", () => {
+    if (mobileMenuButton.classList.contains("active")) {
+      mobileMenuButton.classList.remove("active");
+      mobileMenu.classList.remove("active");
+      document.body.classList.remove("active");
     }
   });
 });
